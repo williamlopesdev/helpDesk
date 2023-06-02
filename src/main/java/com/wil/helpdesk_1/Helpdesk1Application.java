75f164c9efcd1e9fd0f90619f0ff17b30a36796b
williamlopesdev <wilcypherr@gmail.com>
2023-06-02 08:48:57 -0300

    projeto iniciado

---
+++ src/main/java/com/wil/helpdesk_1/Helpdesk1Application.java
@@ -0,0 +1,13 @@
+package com.wil.helpdesk_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helpdesk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helpdesk1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wil/helpdesk_1/Helpdesk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wil.helpdesk_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helpdesk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
