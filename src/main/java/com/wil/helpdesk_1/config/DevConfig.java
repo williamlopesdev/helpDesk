2cd62517fa7139f45165975eefad66cf720f8e2b
williamlopesdev <wilcypherr@gmail.com>
2023-06-03 19:03:05 -0300

    perfil de desenvolvimento

--- src/main/java/com/wil/helpdesk_1/config/DevConfig.java
+++ src/main/java/com/wil/helpdesk_1/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.wil.helpdesk_1.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,14 +9,20 @@ import org.springframework.context.annotation.Profile;
 import com.wil.helpdesk_1.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbservice;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
-	public void instanciaDB() {
-		this.dbservice.instaciaDB();
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbservice.instaciaDB();
+		}
+		return false;
 	}
 }
